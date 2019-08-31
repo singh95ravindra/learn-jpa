929468d0a261673c69ad0a3a83349851f8762c79
singh95ravindra <singh95ravindra@email.com>
2019-08-31 21:07:09 +0530

    added an entity User

---
+++ src/main/java/com/practice/spring/jpa/learnjpa/entity/User.java
@@ -0,0 +1,42 @@
+package com.practice.spring.jpa.learnjpa.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String role;
+
+	protected User() {
+
+	}
+
+	public User(long id, String name, String role) {
+		super();
+		this.name = name;
+		this.role = role;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+
+}
