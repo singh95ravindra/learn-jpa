cf7ce52f3f62d466740f73d58588054ab1fae2a3
singh95ravindra <singh95ravindra@email.com>
2019-09-01 10:16:03 +0530

    few more methods explored.

--- src/main/java/com/practice/spring/jpa/learnjpa/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/practice/spring/jpa/learnjpa/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.practice.spring.jpa.learnjpa;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +22,20 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		log.info("User inserted to database with id : " + userRepository.save(new User("Jack", "Admin")).getId());
+		final User jack = new User("Jack", "Admin");
+		final User jill = new User("Jill", "Admin");
+
+		final User savedUser = userRepository.save(jack);
+		log.info("User inserted to database : " + savedUser);
+
+		final User savedUserJill = userRepository.save(jill);
+		log.info("User inserted to database : " + savedUserJill);
+
+		final Optional<User> findByIdOne = userRepository.findById(1L);
+		log.info("User retrieved : " + findByIdOne);
+
+		final List<User> users = userRepository.findAll();
+		log.info("All users : " + users);
 	}
 
 }
